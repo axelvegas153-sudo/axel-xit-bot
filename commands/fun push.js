571063dc8fba3d7e962403b9835bedf1b8eae9a6
axelvegas153-sudo <axelvegas153@gmail.com>
2026-09-24 08:53:02 -0500

    Create fun push.js

---
+++ commands/fun push.js
@@ -0,0 +1,30 @@
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+   .setName("push")
+   .setDescription("🤜 Empuja a alguien")
+   .addUserOption(option =>
+      option.setName("usuario")
+       .setDescription("A quien quieres empujar")
+       .setRequired(true)),
+  async execute(interaction) {
+    const user = interaction.options.getUser("usuario");
+
+    const gifs = [
+      "https://media.giphy.com/media/l3vR85PnGsBwu1PFK/giphy.gif",
+      "https://media.giphy.com/media/3oGRFZ1aZ7pV0fE2yE/giphy.gif",
+      "https://media.giphy.com/media/xT9IgDEI1PIoCYmkEw/giphy.gif",
+      "https://media.giphy.com/media/11sBLVxU9sEoSQ/giphy.gif"
+    ];
+
+    const randomGif = gifs[Math.floor(Math.random() * gifs.length)];
+
+    const embed = new EmbedBuilder()
+     .setDescription(`**${interaction.user.username}** empujó a **${user.username}** 🤜💥`)
+     .setImage(randomGif)
+     .setColor("Red");
+
+    await interaction.reply({ embeds: [embed] });
+  }
+};
